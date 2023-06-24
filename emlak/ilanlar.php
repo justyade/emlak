3965dac1c041292f09948ac04134587ccb9b3557
justyade <72155075+justyade@users.noreply.github.com>
2023-06-24 16:47:15 +0300

    Update ilanlar.php

--- emlak/ilanlar.php
+++ emlak/ilanlar.php
@@ -11,23 +11,29 @@
 
         #main{height: 100%;}
 
-        .head{height: 11vh; background-color: #E3E5E6;}
+        .sticky{position: fixed; top: 0; left: 0;}
+
+        #head{height: 11vh; background-color: #E3E5E6; width: 100%;}
         #logo{width: 9vh; margin: 1vh; float:left;} 
-        .sub{float: right; width: 4vw; height: 3vh; margin-right: 1vh; margin-top: 2%; }
+        .sub{float: right; width: 4vw; height: 3vh; margin-right: 1vh; margin-top: 2%;}
         .sub:hover{color: grey;}
         #hakkinda{margin-right: 1.4vw;}
         #ara{width: 20vw; height: 2vh; border-radius: 1vw; margin-top: 2%; margin-left: 33%;}
         
-        .filtre{height: 86.8vh; width: 10vw; padding: 0.5%; background-color: #E3E5E6; float: left;}
-        #site, h4{margin: 3px; margin-top: 1vw;}
-        .filtre select{width: 9.5vw; height: 3vh;}
+        #filtre{height: 86.9vh; width: 10vw; padding: 0.5%; background-color: #E3E5E6; float: left; margin-top: 11vh;}
+        #site, #filtre h4{margin: 0.3vh; margin-top: 1vw;}
+        #filtre select{width: 9.5vw; height: 3vh;}
         #min, #max{width: 4vw;}
-        .filtre p{margin: 3px; }
+        #filtre p{margin: 0.3vh;}
+        #filtrele{width: 6vw; height: 3.5vh; border-radius: 1.3vh; border: 1px solid #847474; margin-left: 0.3vh; margin-top: 2vh; background-color: white; font-size: 1.4vh;}
+        #filtrele:hover{background-color: #2132; transition: 0.5s;}
 
+        #icerik{margin-left: 12.5vw; margin-top: 11vh;}
         .ilan, .ilanYatay{border: 1px solid red;}
-        .ilan{width: 10vw; float: left; margin: 2%; padding: 0.3%;}
-        .ilan img{width: 7vw; margin-left: 1.5vw;}
-        h4, p{margin: 0.2%; word-break: break-word;}
+        .ilan{width: 11vw; float: left; margin: 1.5%; padding: 0.3%;}
+        .ilan img{max-width: 11vw; max-height: 20vh;}
+        .ilan h4, .ilan p{margin: 1%; word-break: break-word;}
+        .ilan:hover{background-color: #1221;}
 
         .ilanYatay{width: 18vw; height: 19vh; top: 6vw; margin-left: 11vw;}
         .ilanYatay img{width: 7vw; float: left; margin-right: 1vh;}
@@ -35,14 +41,14 @@
 </head>
 <body>
     <div id="main">
-        <div class="head">
+        <div id="head" class="sticky">
             <img src="tuncEmlak.jpg" id="logo">
             <input type="text" placeholder="Arama..." id="ara">
             <a type="submit" class="sub" value="İlanlar">İlanlar</a>
             <a type="submit" class="sub" value="İletişim">İletişim</a>
             <a type="submit" class="sub" id="hakkinda" value="Hakkında">Hakkında</a>
         </div>
-        <div class="filtre">
+        <div id="filtre" class="sticky">
             <h4>İlçe</h4>
             <select name="ilce" id="ilce">
                 <option value="odunpazari">Odunpazarı</option>
@@ -75,12 +81,128 @@
             <p><input type="checkbox" name="bati" id="bati">Batı</p>
             <p><input type="checkbox" name="kuzey" id="kuzey">Kuzey</p>
             <p><input type="checkbox" name="guney" id="guney">Güney</p>
+
+            <input type="submit" value="Filtrele" id="filtrele">
         </div>
-        <div>
+        <div id="icerik">
             <div class="ilan">
                 <img src="kedyy.jpg" alt="">
-                <h4>Evaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h4>
+                <h4>Ev</h4>
                 <h4>3000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <div><img src="ev.jpeg" alt=""></div>
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+
+            <div class="ilan">
+                <div><img src="ev.jpeg" alt=""></div>
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+
+            <div class="ilan">
+                <div><img src="ev.jpeg" alt=""></div>
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
+                <p>İlan Tarihi:</p>
+                <p>İl/İlçe</p>
+            </div>
+
+            <div class="ilan">
+                <img src="kedyy.jpg" alt="">
+                <h4>Ev</h4>
+                <h4>4000ytl</h4>
                 <p>İlan Tarihi:</p>
                 <p>İl/İlçe</p>
             </div>
